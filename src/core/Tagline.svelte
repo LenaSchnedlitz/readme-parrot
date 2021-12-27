8519ea8649b4e9d195cb7f83aa450c7293f608c8
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2021-12-27 17:41:56 +0100

    Improve iOS gradient color

--- src/core/Tagline.svelte
+++ src/core/Tagline.svelte
@@ -25,7 +25,7 @@
   @supports (background: -webkit-named-image(i)) {
     /* safari only - fixes gradient text */
     p :global(strong) {
-      background: linear-gradient(to right, var(--secondary-color-4) 26%, var(--primary-color) 200%);
+      background: linear-gradient(to right, var(--secondary-color-4) 26%, var(--tertiary-color) 200%);
       background-clip: text;
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
